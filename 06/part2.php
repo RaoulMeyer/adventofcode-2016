b1c3999182f27487ed4d5ce0c194dbf37a6b9ba4
RaoulMeyer <raouljmeyer@gmail.com>
2016-12-06 08:43:08 +0100

    Day 6

---
+++ 06/part2.php
@@ -0,0 +1,20 @@
+<?php
+
+$input = file_get_contents("input");
+
+$occurencesPerLetter = array();
+
+foreach (explode("\n", $input) as $word) {
+    $letters = str_split(trim($word));
+    foreach ($letters as $position => $letter) {
+        if (!isset($occurencesPerLetter[$position][$letter])) {
+            $occurencesPerLetter[$position][$letter] = 0;
+        }
+
+        $occurencesPerLetter[$position][$letter]++;
+    }
+}
+
+foreach ($occurencesPerLetter as $occurences) {
+    echo array_search(min($occurences), $occurences);
+}
